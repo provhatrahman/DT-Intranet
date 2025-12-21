1541b76ae51ad09666e391d1866851d152aad958
provhatrahman <provhatr@gmail.com>
2025-12-21 00:05:23 +0000

    fix: Update revision hash for index.html in service worker to ensure correct caching behavior

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-d2f107b2'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.6k7g9qjshq8"
+    "revision": "0.sp7otjo4pn8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
