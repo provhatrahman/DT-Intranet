0e72cbc21b158f211cd2d03a343d700c2b3ecf09
provhatrahman <provhatr@gmail.com>
2025-12-29 00:41:09 +0000

    c

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-d2f107b2'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.oa0spsl94g"
+    "revision": "0.4pi76aeehr8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
